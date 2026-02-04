1ffad925bd36f58ea573674af076c195750ec98f
Aliane <alianeamaral.tec@gmail.com>
2026-02-04 17:39:58 -0300

    chore: inicia projeto spring executando

---
+++ src/main/java/com/atlaz/atlaz_biblioteca/AtlazBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.atlaz.atlaz_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlazBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlazBibliotecaApplication.class, args);
+	}
+
+}
